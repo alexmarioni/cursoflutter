bb4fbf36f38e59da7627b50f3367a4ffc6164e54
alexmarioni <alexxmarionii@outlook.com>
2025-01-08 16:11:26 -0300

    Añado los archivos de la clase 1 y 2 al repositorio.

---
+++ 01-dart-intro/02_variables.dart
@@ -0,0 +1,19 @@
+void main(){
+  
+  
+  final String pokemon = 'Pablodown';
+  final int hp = 100;
+  final bool isAlive = true;
+  final List<String> abilities = ['impostor'];
+  final List<String> sprites = ['pablodown/front.png',   'pablodown/back.png'];
+  
+  
+  print("""
+  $pokemon
+  $hp
+  $isAlive
+  $abilities
+  $sprites
+  
+  """);
+}
