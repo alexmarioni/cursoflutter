6be2fcbc10917cd41497c0b50ee01c3d727bff34
alexmarioni <alexxmarionii@outlook.com>
2025-01-08 17:06:08 -0300

    Cambio los pokemons y agrego Grego

--- 01-dart-intro/02_variables.dart
+++ 01-dart-intro/02_variables.dart
@@ -1,19 +1,34 @@
-void main(){
-  
-  
+void main() {
   final String pokemon = 'Pablodown';
   final int hp = 100;
   final bool isAlive = true;
   final List<String> abilities = ['impostor'];
-  final List<String> sprites = ['pablodown/front.png',   'pablodown/back.png'];
-  
-  
+  final List<String> sprites = ['pablodown/front.png', 'pablodown/back.png'];
+
+  dynamic errorMessage = 'hola';
+  errorMessage = true;
+  errorMessage = 1;
+  errorMessage = [1, 2, 3, 4, 5];
+  errorMessage = {1, 2, 3, 4, 5, 6};
+  errorMessage = () => true;
+  errorMessage = null;
+
+  final Map<int, dynamic> pokemons = {
+    1: 'Grego',
+    2: 200,
+    3: true,
+    4: 'fire',
+    5: ['grego/front.png', 'grego/back.png']
+  };
+
   print("""
   $pokemon
   $hp
   $isAlive
   $abilities
   $sprites
-  
+  $errorMessage
   """);
+
+  print(pokemons);
 }
