72145ea45eb87557b8ce44b20342e80ad94b2821
Pablo <pablosantangelo2001@hotmail.com>
2025-01-22 18:46:38 -0300

    Agrego try, on exception catch y finally. Elimino archivo basura

--- PabloFineAditions/07_async_await.dart
+++ PabloFineAditions/07_async_await.dart
@@ -1,18 +1,24 @@
 void main() async {
   // Debemos esperar el resultado de httpGet.
-  // Si el main no fuera asíncrono, 
+  // Si el main no fuera asíncrono,
   // no nos detendríamos y continuaríamos ejecutando
-  // antes de obtener el valor de retorno. 
+  // antes de obtener el valor de retorno.
   //Si elimina "async" y "await" notará el erróneo
-  //comportamiento del print(value) 
+  //comportamiento del print(value)
 
   print('Inicio del programa');
 
   try {
     final value = await httpGet('https://fernando-herrera.com/cursos');
     print(value);
+  } on Exception catch(excep){
+    print("""Parece que tenemos una excepción aquí:
+    $excep
+    """);
   } catch (err) {
     print('Tenemos un error: $err');
+  } finally {
+    print('Esto se ejecuta siempre');
   }
 
   print('Fin del programa');
@@ -21,13 +27,14 @@ void main() async {
 // Una función asíncrona no puede retornar otra cosa que no sea un future
 // Explicitar async es necesario para usar await
 Future<String> httpGet(String url) async {
-
   // Ahora que usamos await, la ejecución sí se detendrá
   // para esperar a que se cumpla el future
   await Future.delayed(const Duration(seconds: 2));
-  // y luego se sigue 
+  // y luego se sigue
 
-  throw 'Error en la petición';
-  
+   throw Exception('Acá no sé qué pasó pero es raro');
+
+  // throw 'Error en la petición';
+
   // return 'Tenemos un valor de la petición';
 }
