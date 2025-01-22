2f521f5c741acfa9d26f82f11b08882c875abd40
Pablo <pablosantangelo2001@hotmail.com>
2025-01-22 18:34:25 -0300

    Acá seguimos progresando amigo

---
+++ PabloFineAditions/07_async_await.dart
@@ -0,0 +1,33 @@
+void main() async {
+  // Debemos esperar el resultado de httpGet.
+  // Si el main no fuera asíncrono, 
+  // no nos detendríamos y continuaríamos ejecutando
+  // antes de obtener el valor de retorno. 
+  //Si elimina "async" y "await" notará el erróneo
+  //comportamiento del print(value) 
+
+  print('Inicio del programa');
+
+  try {
+    final value = await httpGet('https://fernando-herrera.com/cursos');
+    print(value);
+  } catch (err) {
+    print('Tenemos un error: $err');
+  }
+
+  print('Fin del programa');
+}
+
+// Una función asíncrona no puede retornar otra cosa que no sea un future
+// Explicitar async es necesario para usar await
+Future<String> httpGet(String url) async {
+
+  // Ahora que usamos await, la ejecución sí se detendrá
+  // para esperar a que se cumpla el future
+  await Future.delayed(const Duration(seconds: 2));
+  // y luego se sigue 
+
+  throw 'Error en la petición';
+  
+  // return 'Tenemos un valor de la petición';
+}
